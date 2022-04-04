91af4ce0840d144de46f0aeaae1df8fed44138ef
实名晚上冲浪 <dkjusting@outlook.com>
2022-04-05 00:24:35 +0800

    去掉了关键词和白名单功能 把判断是否存在改成先读RSS的列表（由于notion每次只返回100个数据），然后将所有的entry的URL整成一个列表，进行比较。

--- feedtool.py
+++ feedtool.py
@@ -8,21 +8,6 @@ import time
 NOTION_PARA_BLOCK_LIMIT = 2000
 now = time.time()
 
-"""
-entry = {
-    "title"  : "文章标题",
-    "link"   : "文章链接",
-    "summary": "文章摘要",
-    "synced" : False,
-    "time"   : "发布时间",
-    "rss"    : {
-        "title"      : "RSS 标题",
-        "url"        : "RSS 地址",
-        "isWhiteList": "是否白名单"
-    }
-}
-"""
-
 
 def parse_rss(rss_info: dict):
     entries = []
@@ -36,7 +21,6 @@ def parse_rss(rss_info: dict):
         print("Feedparser error")
         return []
     for entry in feed.entries:
-        # if hasattr(entry, published_parsed):
         if now - time.mktime(entry.published_parsed) < (7 * 24 * 3600):
             entries.append(
                 {
@@ -48,12 +32,8 @@ def parse_rss(rss_info: dict):
                     "rss": rss_info,
                 }
             )
-    # 读取前 20 条
-    return entries[:20]
-
-
-def html2Notion(html: str):
-    pass
+    # 读取前 100 条
+    return entries[:100]
 
 
 def deep_get(dictionary, keys, default=None):
@@ -67,10 +47,9 @@ def deep_get(dictionary, keys, default=None):
 class NotionAPI:
     NOTION_API_HOST = "https://api.notion.com/v1"
 
-    def __init__(self, sec, rss, keyword, coll) -> None:
+    def __init__(self, sec, rss, coll) -> None:
         self._sec = sec
         self._rss_id = rss
-        self._kw_id = keyword
         self._col_id = coll
         self.HEADERS = {
             "Authorization": f"Bearer {self._sec}",
@@ -80,18 +59,8 @@ class NotionAPI:
         self.session = _req.Session()
         self.session.headers.update(self.HEADERS)
 
-    def api_endpoint(self, path):
-        return "{}{}".format(self.NOTION_API_HOST, path)
-
-    def query_keywords(self):
-        api = self.api_endpoint(f"/databases/{self._kw_id}/query")
-        res = self.session.post(api, json={"filter": {"property": "Open", "checkbox": {"equals": True}}})
-        results = res.json().get("results")
-        keyword_list = [deep_get(k, "properties.KeyWords.title")[0].get("text").get("content") for k in results]
-        return keyword_list
-
     def query_open_rss(self):
-        api = self.api_endpoint(f"/databases/{self._rss_id}/query")
+        api = self.NOTION_API_HOST + f"/databases/{self._rss_id}/query"
         res = self.session.post(
             api,
             json={"filter": {"property": "Enable", "checkbox": {"equals": True}}},
@@ -99,7 +68,6 @@ class NotionAPI:
         results = res.json().get("results")
         rss_list = [
             {
-                "isWhiteList": deep_get(r, "properties.Whitelist.checkbox"),
                 "url": deep_get(r, "properties.URL.url"),
                 "title": deep_get(r, "properties.Name.title")[0].get("text").get("content"),
             }
@@ -107,33 +75,18 @@ class NotionAPI:
         ]
         return rss_list
 
-    def is_page_exist(self, url):
-        api = self.api_endpoint(f"/databases/{self._col_id}/query")
-        res = self.session.post(api, json={"filter": {"property": "URL", "text": {"equals": url}}})
-        return len(res.json().get("results")) > 0
-
     def save_page(self, entry):
-        api = self.api_endpoint("/pages")
+        api = self.NOTION_API_HOST + "/pages"
 
         title = entry.get("title")
         summary = entry.get("summary")
-
-        multi_selects = [{"name": kw} for kw in entry.get("match_keywords")]
-
-        # NOTION API 限制 Summary 长度：
-        """
-        body.children[1].paragraph.text[0].text.content.length should be ≤ `2000`
-        """
 
         data = {
             "parent": {"database_id": self._col_id},
             "properties": {
                 "标题": {"title": [{"text": {"content": title}}]},
                 "URL": {"url": entry.get("link")},
-                "关键词": {"multi_select": multi_selects},
-                # "Entropy": {"number": entry.get("entropy", 0.0)},
                 "来源": {"rich_text": [{"text": {"content": entry.get("rss").get("title")}}]},
-                "白名单": {"checkbox": entry.get("rss").get("isWhiteList")},
                 "发布时间": {"date": {"start": entry.get("time")}},
             },
             "children": [
--- main.py
+++ main.py
@@ -1,81 +1,35 @@
 import os
-import requests
-from feedtool import NotionAPI, deep_get, parse_rss
+from feedtool import NotionAPI, parse_rss
 
 NOTION_SEC = os.environ.get("NOTION_SEC")
 NOTION_DB_RSS = os.environ.get("NOTION_RSS")
-NOTION_DB_KEYWORDS = os.environ.get("NOTION_KEY")
 NOTION_DB_READER = os.environ.get("NOTION_RED")
-
 FEISHU_BOT_API = os.environ.get("FEISHU_BOT_API")
 
 
-def feishu_bot_send_msg(msg):
-    """
-    msg = {"title": "", "content": ""}
-    """
-    if FEISHU_BOT_API:
-        requests.post(FEISHU_BOT_API, json={"msg_type": "post", "content": {"post": {"zh_cn": msg}}})
-
-
-def _wrap_rss_warning_msg_fmt(title, url):
-    content = f"{title} 读取失败！\n\t{url}"
-    feishu_bot_send_msg({"title": "❗ RSS Warning", "content": [[{"tag": "text", "text": content}]]})
-
-
-def process_entry(entry: dict, keywords: list):
-    entropy = 0
-    match_keywords = []
-    # TODO: filter keywords -
-    text = f'{entry.get("title")} {entry.get("summary")}'
-    for kw in keywords:
-        if kw in text:
-            print(f"Keyword {kw} Matched! -> #{entry.get('title')}")
-            match_keywords.append(kw)
-            entropy += 1
-
-    if len(keywords) > 0:
-        entropy /= len(keywords)
-
-    if deep_get(entry, "rss.isWhiteList"):
-        entropy = 1
-
-    entry["entropy"] = float(f"{entropy}")
-    entry["match_keywords"] = match_keywords
-
-    return entry
-
-
-def read_rss(rsslist):
-    for rss in rsslist:
+def read_rss(api):
+    for rss in api.query_open_rss():
         # !! 必须和 Notion RSS DB 保持一致
         entries = parse_rss(rss)
         print(f"Got {len(entries)} items from #{rss.get('title')}#")
         if len(entries) == 0:
-            # 飞书提示
-            _wrap_rss_warning_msg_fmt(rss.get("title"), rss.get("url"))
+            continue
+        data = api.session.post(api.NOTION_API_HOST + f"/databases/{api._col_id}/query", json={"filter": {"property": "来源", "text": {"equals": entries[0].get("rss").get("title")}}})
+        urls = [x.get("properties").get("URL").get("url") for x in data.json().get("results")]
         for entry in entries:
-            yield entry
+            if entry.get("link") not in urls:
+                api.save_page(entry)
+            else:
+                print(f"Entry {entry.get('title')} already exist!")
 
 
 def run():
     if NOTION_SEC is None:
         print("NOTION_SEC secrets is not set!")
         return
-    api = NotionAPI(NOTION_SEC, NOTION_DB_RSS, NOTION_DB_KEYWORDS, NOTION_DB_READER)
+    api = NotionAPI(NOTION_SEC, NOTION_DB_RSS, NOTION_DB_READER)
 
-    keywords = api.query_keywords()
-
-    for entry in read_rss(api.query_open_rss()):
-        res = process_entry(entry, keywords)
-        if res.get("entropy") > 0:
-            if not api.is_page_exist(entry.get("link")):
-                api.save_page(entry)
-            else:
-                print(f"Entry {entry.get('title')} already exist!")
-    # 飞书提示
-    # if len(new_entries) > 0:
-    #     _wrap_rss_new_msg_fmt(new_entries)
+    read_rss(api)
 
 
 if __name__ == "__main__":
